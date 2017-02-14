d2e0aba844f7a00c95bce7947ce4ca3f50ef6b2f
gabripo <gabripo93+github@gmail.com>
2017-02-14 11:25:03 +0100

    Analog input A0 reading and logging
    Signed-off-by: gabripo <gabripo93+github@gmail.com>

--- src/modules/interface/ctrtask.h
+++ src/modules/interface/ctrtask.h
@@ -24,12 +24,13 @@
 // Including the processor tools
 #include "stm32fxxx.h"
 
-#include "sound.h"
+// Including the log utility
+#include "log.h"
 
 // Custom macros
 #define CTR_TASK_NAME           "CTR-TASK"
 #define CTR_TASK_PRI            0
-#define PERIOD_SLEEP_TIME_IN_MS 5000
+#define PERIOD_SLEEP_TIME_IN_MS 1000
 
 // Function definitions
 void customTaskCreator(void);
--- src/modules/src/ctrtask.c
+++ src/modules/src/ctrtask.c
@@ -7,6 +7,9 @@
 // Pre-defining functions
 static void customTask(void *arg);
 
+// Read variable
+static float value = 0;
+
 void customTaskCreator(void) {
 
 	xTaskCreate(customTask, CTR_TASK_NAME,
@@ -18,6 +21,7 @@ void customTaskCreator(void) {
 void customTask(void *arg) {
   // Waiting until the system task completely starts
 	systemWaitStart();
+	DEBUG_PRINT("Starting the CTR-TASK..");
 
   // Defining ticks number
   TickType_t xLastWakeTime;
@@ -26,13 +30,21 @@ void customTask(void *arg) {
 		  // Getting the ticks number in this moment (when the function starts)
 		  xLastWakeTime = xTaskGetTickCount();
 
-		  // STARTING THE TASK
+		  /* STARTING THE TASK */
 
-		  // Starting to play a melody after 4 seconds
-		  vTaskDelay(M2T(4000));
-		  soundSetEffect((uint32_t) 11);
+		  // Defining the pin input, referring to the deck_constants.h file - A0
+			pinMode(DECK_GPIO_TX2, INPUT);
+		  value = analogRead(DECK_GPIO_TX2);
 
-		  // ENDING THE TASK
+		  // Printing on the console (double conversion is mandatory because is the only allowed variable type from DEBUG_PRINT)
+		  DEBUG_PRINT("CTR-TASK read value: %f \n", (double) value);
+
+		  // Adding to the client parameters
+			LOG_GROUP_START(CTR)
+			LOG_ADD(LOG_FLOAT, readValue, &value)
+			LOG_GROUP_STOP(CTR)
+
+		  /* ENDING THE TASK */
 
 		  // Wait execution repeating until PERIOD_SLEEP_TIME_IN_MS is elapsed
 		  vTaskDelayUntil(&xLastWakeTime, M2T(PERIOD_SLEEP_TIME_IN_MS));
